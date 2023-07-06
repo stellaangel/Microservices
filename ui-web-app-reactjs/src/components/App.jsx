4d0c6a2b03f4590d30743bdc45f604f0411d2817
Kiruba1931 <122162815+Kiruba1931@users.noreply.github.com>
2023-07-07 02:25:37 +0530

    Update App.jsx

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
 
   const handleApiCall = async (e) => {
     // const url = 'http://localhost:9999/'+e.target.name;
-    let url = 'http://3.84.234.190:32470/'+e.target.name
+    let url = 'http://54.158.82.164:32470/'+e.target.name
 
     displayData.url=url
     let options = {}
