38a38428995fce63fadeb497bfbb86eafd790de1
stellaangel <124707914+stellaangel@users.noreply.github.com>
2023-07-10 16:04:32 +0530

    Update App.jsx

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
 
   const handleApiCall = async (e) => {
     // const url = 'http://localhost:9999/'+e.target.name;
-    let url = 'http://3.84.50.57:32470/'+e.target.name
+    let url = 'http://35.173.49.196:32470/'+e.target.name
 
     displayData.url=url
     let options = {}
